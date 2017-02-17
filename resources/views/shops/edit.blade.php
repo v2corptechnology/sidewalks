30b76f9fa5da972418c4d7b8af94581fc4990c9d
Romain Sauvaire <romain.sauvaire@gmail.com>
2017-02-17 10:43:59 +0100

    [fix] Inputs are not aligned

--- resources/views/shops/edit.blade.php
+++ resources/views/shops/edit.blade.php
@@ -15,28 +15,28 @@
                         @if (! request()->exists('narrow'))
 
                             <div class="form-group {{ $errors->has('phone') ? ' has-error' : '' }}">
-                                <label class="control-label col-sm-3" for="phone">Shop phone</label>
+                                <label class="control-label col-sm-4" for="phone">Shop phone</label>
                                 <div class="col-sm-4">
                                     <input class="form-control" name="phone" value="{{ old('phone', $shop->phone) }}" id="phone" type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" placeholder="+1 665-555-1" required>
                                     {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
                                 </div>
                             </div>
                             <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label class="control-label col-sm-3" for="email">Shop email</label>
+                                <label class="control-label col-sm-4" for="email">Shop email</label>
                                 <div class="col-sm-4">
                                     <input class="form-control" name="email" value="{{ old('email', $shop->email) }}" id="email" type="email" placeholder="wonderful@shop.com" required>
                                     {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
                                 </div>
                             </div>
                             <div class="form-group {{ $errors->has('address') ? ' has-error' : '' }}">
-                                <label class="control-label col-sm-3" for="address">Shop address</label>
+                                <label class="control-label col-sm-4" for="address">Shop address</label>
                                 <div class="col-sm-4">
                                     <input class="form-control" name="address" value="{{ old('address', $shop->address) }}" id="address" type="text" placeholder="1 Quality Street, 99110 Ca" required>
                                     {!! $errors->first('address', '<p class="help-block">:message</p>') !!}
                                 </div>
                             </div>
                             <div class="form-group {{ $errors->has('contact') ? ' has-error' : '' }}">
-                                <label class="control-label col-sm-3" for="contact">Contact name</label>
+                                <label class="control-label col-sm-4" for="contact">Contact name</label>
                                 <div class="col-sm-4">
                                     <input class="form-control" name="contact" value="{{ old('contact', $shop->contact) }}" id="contact" type="text" placeholder="Mister Owner" required>
                                     {!! $errors->first('contact', '<p class="help-block">:message</p>') !!}
