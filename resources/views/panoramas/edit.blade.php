1e12ab530f9b6f03b5e798640fe00d44e5ee7617
Romain Sauvaire <romain.sauvaire@gmail.com>
2017-03-01 18:59:47 +0100

    Display link to go back to panorama

--- resources/views/panoramas/edit.blade.php
+++ resources/views/panoramas/edit.blade.php
@@ -7,8 +7,11 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-8">
-            <h1>Current view <small>/ {{ $panorama->path->name }}</small></h1>
-            <path-viewer view-id="{{ $panorama->id }}" height="300" editable></path-viewer>
+            <h1>
+                <a href="{{ route('paths.edit', $panorama->path) }}">{{ $panorama->path->name }}</a> 
+                <small>/ Panorama {{ $panorama->id }}</small>
+            </h1>
+            <path-viewer view-id="{{ $panorama->id }}" height="500" editable></path-viewer>
         </div>
         <div class="col-sm-4">
             <h1>Linked views</h1>
