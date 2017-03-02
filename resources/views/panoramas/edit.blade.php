23f285d5faf15686a8d49b1b0e0416177120fc30
Romain Sauvaire <romain.sauvaire@gmail.com>
2017-03-02 17:58:30 +0100

    Display link to view panorama as "public"

--- resources/views/panoramas/edit.blade.php
+++ resources/views/panoramas/edit.blade.php
@@ -8,8 +8,12 @@
     <div class="row">
         <div class="col-sm-8">
             <h1>
-                <a href="{{ route('paths.edit', $panorama->path) }}">{{ $panorama->path->name }}</a> 
+                <a href="{{ route('paths.edit', $panorama->path) }}" title="Back to path panoramas">{{ $panorama->path->name }}</a> 
                 <small>/ Panorama {{ $panorama->id }}</small>
+
+                <a class="pull-right h4" href="{{ route('panoramas.show', $panorama) }}" title="View full path">
+                    <small><i class="fa fa-refresh"></i> visit</small>
+                </a>
             </h1>
             <path-viewer view-id="{{ $panorama->id }}" height="500" editable></path-viewer>
         </div>
