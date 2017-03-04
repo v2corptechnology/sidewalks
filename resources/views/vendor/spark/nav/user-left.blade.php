5305059ee8e413758674d952a93170a862fa5ca3
Romain Sauvaire <romain.sauvaire@gmail.com>
2017-03-04 10:45:23 +0100

    Display item creation in menue, hide shop-related things

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -1,21 +1,18 @@
 <!-- Left Side Of Navbar -->
-@if (auth()->user()->hasShop())
-    <li>
-        <a href="{{ route('shops.show', auth()->user()->shop) }}">My shop</a>
-    </li>
-    <li>
-        <a href="{{ route('items.create') }}">Create item</a>
-    </li>
-@else
-    <li>
-        <a href="{{ route('shops.create') }}">Create my shop</a>
-    </li>
-@endif
-
+<li>
+    <a href="{{ route('items.create') }}">Create item</a>
+</li>
 <li>
     <a href="{{ route('paths.index') }}">Paths</a>
 </li>
-
-<li class="hidden">
+ {{-- 
+<li>
+    <a href="{{ route('shops.show', auth()->user()->shop) }}">My shop</a>
+</li>
+<li>
+    <a href="{{ route('shops.create') }}">Create my shop</a>
+</li>
+<li>
     <a href="{{ route('crawls.create') }}">Crawl website</a>
 </li>
+--}}
